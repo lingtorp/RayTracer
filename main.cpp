8a83b4cf6c7974630332129ddc65b510107247b0
Alexander Lingtorp <AlexanderLingtorp@Alexanders-MBP.fritz.box>
2018-03-30 18:30:27 +0200

    Added rudimentary SDL2 support for viewing the images.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
 #include "vector.h"
 #include "world.h"
 #include "sphere.h"
@@ -26,26 +28,33 @@ Vec3<> color(const Ray& r, World& world, int depth) {
 }
 
 int main() {
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  bool quit = false;
+  size_t nx = 400;
+  size_t ny = 200;
+  size_t ns = 10; // Number of samples per px
+  Camera cam{120, double(nx) / double(ny)};
+
+  SDL_WindowFlags flags = SDL_WINDOW_SHOWN;
+  SDL_Window* window = SDL_CreateWindow("RayTracer", 0, 0, nx, ny, flags);
+  SDL_Surface* scr = SDL_GetWindowSurface(window);
+  std::cout << (SDL_GetWindowPixelFormat(window) == SDL_PIXELFORMAT_ARGB8888) << std::endl;
+  uint32_t* pixels = (uint32_t*) scr->pixels;
+  std::cout << (scr->format->format == SDL_PIXELFORMAT_ARGB8888) << std::endl;
+
   std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
   std::default_random_engine engine{static_cast<unsigned int>(tp.time_since_epoch().count())};
   std::uniform_real_distribution<double> distribution(0.0, 1.0); // 0.0 <= x < 1.0
   auto rand = std::bind(distribution, engine);
-  
-  time_t start = std::time(nullptr);
-  
-  std::ofstream image;
-  image.open("gradient.ppm");
-  image << "P3" << '\n';
-  size_t nx = 400;
-  size_t ny = 200;
-  size_t ns = 100; // Number of samples per px
-  image << nx << " " << ny << '\n' << 255 << '\n';
-  Camera cam{120, double(nx) / double(ny)};
+
   World world;
   world.hitables.push_back(new Sphere{Vec3<>{0, 0, -1}, 0.5, new Lambertian{0.8, 0.3, 0.3}});
   world.hitables.push_back(new Sphere{Vec3<>{0, -100.5, -1}, 100, new Lambertian{0.8, 0.8, 0.0}});
   world.hitables.push_back(new Sphere{Vec3<>{1, 0, -1}, 0.5, new Metal{0.8, 0.8, 0.0, 0.3}});
   world.hitables.push_back(new Sphere{Vec3<>{-1, 0, -1}, 0.5, new Dielectric{1.5}});
+
+  time_t start = std::time(nullptr);
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       Vec3<> t_color{};
@@ -53,22 +62,31 @@ int main() {
         double u = double(i + rand()) / double(nx);
         double v = double(j + rand()) / double(ny);
         Ray r = cam.get_ray(u, v);
-  
+
         Vec3<> p = r(2.0);
         t_color += color(r, world, 0);
       }
       t_color /= double(ns);
       t_color = {std::sqrt(t_color.x), std::sqrt(t_color.y), std::sqrt(t_color.z)}; // Gamma-2 correction
-      int ir = int(t_color.x * 255);
-      int ig = int(t_color.y * 255);
-      int ib = int(t_color.z * 255);
-      image << ir << " " << ig << " " << ib << '\n';
+      auto ir = uint32_t(t_color.x * 255);
+      auto ig = uint32_t(t_color.y * 255);
+      auto ib = uint32_t(t_color.z * 255);
+      auto ia = uint32_t(1);
+      uint32_t pixel = 0;
+      pixel += (ia << (8 * 3));
+      pixel += (ir << (8 * 2));
+      pixel += (ig << (8 * 1));
+      pixel += (ib << (8 * 0));
+      pixels[(j * nx) + i] = pixel;
     }
   }
   
   time_t end = std::time(nullptr);
   double diff = std::difftime(end, start);
   std::cout << "Finished in: " << diff << " seconds." << std::endl;
+
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(20000);
   
   return 0;
 }
