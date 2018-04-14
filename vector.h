5d993369af011ac006c1f2c6ebdde68c6ac9e377
Alexander Lingtorp <AlexanderLingtorp@Alexanders-MBP.fritz.box>
2018-04-14 08:56:37 +0200

    Made it all use threads.
    From 600krays/s to 1.9Mrays/s on 8 threads for a utilisation of just
    under 40% of the theoretical peak performance.
    
    1900000/(600000 * 8) < 0.40

--- vector.h
+++ vector.h
@@ -65,10 +65,10 @@ Vec3<> reflect(const Vec3<>& v, const Vec3<>& n) {
 }
 
 Vec3<> random_in_unit_sphere() {
-  std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
-  std::default_random_engine engine{static_cast<unsigned int>(tp.time_since_epoch().count())};
-  std::uniform_real_distribution<double> distribution(0.0, 1.0); // 0.0 <= x < 1.0
-  auto rand = std::bind(distribution, engine);
+  static std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
+  static std::default_random_engine engine{static_cast<unsigned int>(tp.time_since_epoch().count())};
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0); // 0.0 <= x < 1.0
+  static auto rand = std::bind(distribution, engine);
   Vec3<> p;
   do {
     p = 2.0 * Vec3<>{rand(), rand(), rand()} - Vec3<>{1.0};
